8cc16eec756c9a710efb6c78cd5023376fd9cd69
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2018-08-22 08:13:16 +0530

    Webview Added
    Webview Added

--- app/src/main/java/com/k5financial/k5app/HTLanding.java
+++ app/src/main/java/com/k5financial/k5app/HTLanding.java
@@ -5,44 +5,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.RadioButton;
 
 public class HTLanding extends AppCompatActivity {
 
-    RadioButton sf;
-    RadioButton sands;
-    RadioButton ff;
+//    RadioButton sf;
+//    RadioButton sands;
+//    RadioButton ff;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_htlanding);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        sf = (RadioButton)findViewById(R.id.selfradio);
-        sands = (RadioButton)findViewById(R.id.radiosands);
-        ff = (RadioButton)findViewById(R.id.radioff);
+        final WebView wv = (WebView)findViewById(R.id.webviewone);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
-        sf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), SelfHT.class);
-                startActivity(i);
-            }
-        });
-
-        sands.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        ff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        wv.loadUrl("https://www.journaldev.com");
+        wv.loadUrl("https://happy.adityabirlacapital.com/abhiqq/#quickQuote");
+//
+//        sf = (RadioButton)findViewById(R.id.selfradio);
+//        sands = (RadioButton)findViewById(R.id.radiosands);
+//        ff = (RadioButton)findViewById(R.id.radioff);
+//
+//        sf.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getApplicationContext(), SelfHT.class);
+//                startActivity(i);
+//            }
+//        });
+//
+//        sands.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//
+//        ff.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
 
     }
     @Override
